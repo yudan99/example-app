1b244a873a81358541626785791c40c2f918f365
Your Name <you@example.com>
2022-03-23 08:36:05 +0000

    jichuyemian

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/','StaticPagesController@home');
+Route::get('help/','StaticPagesController@help');
+Route::get('about/','StaticPagesController@about');
+//Route::get('/','App\Http\Controllers\StaticPagesController@home');
+//Route::get('/','app/Http/Controllers/StaticPagesController@home');
